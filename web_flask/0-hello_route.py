86ba2e97096461e03706d16414311b21cb3bbf7c
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-08-18 02:01:10 +0300

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,15 @@
 '''flask simple response'''
 from flask import Flask
 
-app =  Flask(__name__)
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def Hello_HBNB(strict_slashes=False):
     ''' function that act as api response '''
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
