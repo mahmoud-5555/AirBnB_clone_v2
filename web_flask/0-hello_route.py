344e60503a2f5761ff668c80dc76db142a02ccb8
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-08-17 11:30:39 +0300

    test

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app =  Flask(__name__)
+
+@app.route('/')
+def Hello_HBNB(strict_slashes=False):
+    ''' function that act as api response '''
+    return 'Hello HBNB!'
+
+if "__name__" == "__main__":
+    app.run(host='0.0.0.0', port='5000')
