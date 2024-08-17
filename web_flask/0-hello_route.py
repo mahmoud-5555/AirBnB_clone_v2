d7f4d4d971464649620a9dab7be3dd8209dc7854
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-08-18 01:08:34 +0300

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,5 +9,5 @@ def Hello_HBNB(strict_slashes=False):
     ''' function that act as api response '''
     return 'Hello HBNB!'
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+# This is the Fist time that tru flask
