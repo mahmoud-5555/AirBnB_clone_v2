c59f7892e24efffe7442be5e6df9da489a6cb26c
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-08-18 03:48:06 +0300

    test

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''flask simple response'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def Hello_HBNB(strict_slashes=False):
+    ''' function that act as api response '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB(strict_slashes=False):
+    ''' function that act as api response '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def C(text, strict_slashes=False):
+    ''' function that act as api response '''
+    return 'C ' + text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
