daffd7116401c3053de4516296e04e216200efeb
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-08-29 14:23:28 +0300

    test

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,14 +15,35 @@ def reconect_DP(exception=None):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def allStatesView():
     from models.state import State
+    from models.city import City
     ''''method to response all states request'''
-    data = storage.all(State)
-    data = sorted(data.values(), key=lambda a: a.name)
-    return render_template('', respo=data)
-
+    data_states = storage.all(State)
+    data_cities =  storage.all(City)
+    data_results = [] 
+    """
+    data_results:
+    type : list of dict "states" | key(tuple("id of the state"
+    , "name of the staes")) | value("list of tuple(city's id, city's name)")
+    """
+    # group the data
+    for i in data_states:
+        element = dict() # var to make an states  
+        key = tuple(i.id, i.name)
+        element[key] = []  #State element
+        for j in data_cities:
+            if i.id == j.state_id:
+				# add new value to the states | City : tuple(id , name)
+                element[key].append(tuple(j.id, j.name))
+        # After done of the element sort the cities in side the states 
+        element[key] = sorted(element[key], key= lambda a: a[1])
+        data_results.append(element)
+    # Sort the States  
+    data_results = sorted(data_results, key= lambda a: a.key()[0][1])
+    
+    return render_template('', respo=data_results)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
